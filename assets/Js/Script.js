71a2e786ecbd77f6438d482e9f91074e64a3e553
Raish0009 <raishmohammad009@gmail.com>
2025-08-04 10:11:14 +0530

    Furniro

---
+++ assets/Js/Script.js
@@ -0,0 +1,14 @@
+
+  var swiper = new Swiper('.inspirations-swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
